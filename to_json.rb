6dee3a8196ace4c43db469d87c7a72512c4a4a06
Matt Willhite <matthew.willhite@gmail.com>
2010-12-03 10:03:14 -0800

    Pushes the status code through the stack

--- to_json.rb
+++ to_json.rb
@@ -2,28 +2,30 @@ class ToJson
   
   def call env
     @request  = Rack::Request.new env
-    response = Rack::Response.new
-    response['Content-Type'] = 'application/json'
+    
+    @response = Rack::Response.new
+    @response['Content-Type'] = 'application/json'
   
-    # http://www.earthtools.org/sun/45.505242/-122.595212/3/12/-0800/1
+    # i.e. http://www.earthtools.org/sun/45.505242/-122.595212/3/12/-0800/1
     xml_response = if @request['q']
       parse_xml(@request['q'])
     else
-      { :error => "'q' is a required parameter" }
+      { :error => "requires parameter 'q' to contain a url that returns an xml response" }
     end
   
-    response.write xml_response.to_json
+    @response.write xml_response.to_json
   
-    response.finish
+    @response.finish
   end
   
   private
   
-    def parse_xml(params)
+    def parse_xml(params) 
       begin
-        Crack::XML.parse(Net::HTTP.get_response(URI.parse(@request['q'])).body)
+        api_response = Net::HTTP.get_response(URI.parse(@request['q']))
+        Crack::XML.parse(api_response.body) unless api_response.value # Raises an error unless status is 2xx
       rescue Exception => e
-        { :error => e }
+        @response.status = e.message.to_i # Push the status code
       end
     end
 end
